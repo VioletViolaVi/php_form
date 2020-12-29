a089cdedff436be7e1ca4ad46582677b35ef920f
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-29 15:31:15 +0000

    Add form and separate page for action attribute.

---
+++ form1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Form</title>
+</head>
+<body>
+    <form method="GET" action="form1_confirmation.php">
+        <input type="text" name="fname" placeholder="first name">
+        <input type="text" name="lname" placeholder="last name">
+        <button type="submit">submit</button>        
+    </form>
+</body>
+</html>
