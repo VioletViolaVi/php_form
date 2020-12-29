ccd770ff673069d1d319d2644659589b7eed76d8
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-29 15:32:34 +0000

    Add reset link for form.

--- form1.php
+++ form1.php
@@ -9,7 +9,8 @@
     <form method="GET" action="form1_confirmation.php">
         <input type="text" name="fname" placeholder="first name">
         <input type="text" name="lname" placeholder="last name">
-        <button type="submit">submit</button>        
+        <button type="submit">submit</button> 
+        <a href="form1.php">Reset</a>       
     </form>
 </body>
 </html>
