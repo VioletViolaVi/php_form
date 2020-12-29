ab62d5684eadf7d8fa0b6d69ede5f59bf1a87e3d
VioletViolaVi <vivian.v18723@gmail.com>
2020-12-29 15:45:31 +0000

    Make form send inputted data to separate page.

--- form1.php
+++ form1.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Form</title>
 </head>
+
 <body>
     <form method="GET" action="form1_confirmation.php">
         <input type="text" name="fname" placeholder="first name">
         <input type="text" name="lname" placeholder="last name">
-        <button type="submit">submit</button> 
-        <a href="form1.php">Reset</a>       
+        <button type="submit">submit</button>
+        <a href="form1.php">Reset</a>
     </form>
+    <br>
+    <form method=""></form>
 </body>
+
 </html>
